bd51c905dc77fe52d62a19bfc21f001113b65fda
flam1ngo <kris.s.03@mail.ru>
2024-05-24 10:37:43 +0300

    Initial commit

---
+++ TemplateTask1/TemplateTask1.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+template <typename T>
+T mod(T a) {
+
+    return a * a;
+}
+
+template <typename T>
+std::vector<T> mod(std::vector<T>& a) {
+    for (auto& i : a)
+        i *= i;
+    return a;
+}
+
+int main()
+{
+    int num = 4;
+    
+    std::cout << "[IN]: " << num << std::endl;
+    std::cout << "[OUT]: " << mod(num) << std::endl;
+
+
+    std::vector<int> vec = { -3, 4, 1, 6, 4, -7 };
+    std::cout << "[IN]: ";
+
+    std::for_each(vec.begin(), vec.end() - 1, [](const int& n) { std::cout << n << ", "; });
+    std::cout << *(vec.end() - 1) << std::endl;
+
+    mod(vec);
+
+    std::cout << "[OUT]: ";
+    std::for_each(vec.begin(), vec.end() - 1, [](const int& n) { std::cout << n << ", "; });
+    std::cout << *(vec.end() - 1) << std::endl;
+
+    return 0;
+}
